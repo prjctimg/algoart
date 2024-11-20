ddf2858549eb75c1d7f0c9cd5dcdb5b4d1bbe7fc
ディーン・タリサイ 🌊 <arcaneqoder@gmail.com>
2024-11-20 10:57:19 +0200

    I think I know wtf is going on...onward

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,29 +1,9 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
-
+ 
 export default function Home() {
-  const count = useSignal(3);
+
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Why won't you work</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
 
-        <br />
-
-        <button class='btn btn-primary'>Click me</button>
-      </div>
     </div>
   );
 }
